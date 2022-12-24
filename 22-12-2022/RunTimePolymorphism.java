39876565635140137b9d3d1afbf57044d73e91c4
Palaksingh1 <117344679+Palaksingh1@users.noreply.github.com>
2022-12-24 17:41:31 +0530

    Add files via upload

---
+++ 22-12-2022/RunTimePolymorphism.java
@@ -0,0 +1,54 @@
+package multithreading;
+
+class Creature
+{
+	void eat()
+	{
+		System.out.println("Creatures eat");
+	}
+}
+
+class Herbivores extends Creature
+{
+	void eat()
+	{
+		System.out.println("Herbivores eat plants");
+	}
+}
+
+class Omnivores extends Creature
+{
+	void eat()
+	{
+		System.out.println("Omnivores eat plants and meat");
+	}
+}
+
+class Carnivores extends Creature
+{
+	void eat()
+	{
+		System.out.println("Carnivores eat meat");
+	}
+}
+public class RunTimePolymorphism {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Creature C=new Creature();
+		Creature h=new Herbivores();
+		
+		//upcasting
+		Creature o=new Omnivores();
+		//upcasting
+		Creature c=new Carnivores();
+		
+		//upcasting
+		C.eat();
+		h.eat();
+		o.eat();
+		c.eat();
+		
+	}
+
+}
