e88602de63649b49114618933c7219b1d77b1385
Palaksingh1 <117344679+Palaksingh1@users.noreply.github.com>
2022-12-24 17:51:00 +0530

    Add files via upload

---
+++ 22-12-2022/VaccinationException.java
@@ -0,0 +1,16 @@
+package multithreading;
+
+public class VaccinationException extends Exception {
+
+	public VaccinationException(String message)
+	{
+		super(message);
+	}
+
+	public static void eligibility(String gender, int age, String nationality) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
